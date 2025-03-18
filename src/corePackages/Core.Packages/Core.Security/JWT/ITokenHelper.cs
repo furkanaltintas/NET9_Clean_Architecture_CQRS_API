bcf5ccf3909efcc57bc7c983aa06edc854ffe7fe
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-18 17:12:37 +0300

    Access ve Refresh Token implementasyonları

---
+++ src/corePackages/Core.Packages/Core.Security/JWT/ITokenHelper.cs
@@ -0,0 +1,11 @@
+﻿using Core.Security.Entities;
+
+namespace Core.Security.JWT;
+
+public interface ITokenHelper
+{
+    // Kullanıcının user datası ve o kullanıcıya ait claimleri
+    AccessToken CreateToken(User user, IList<OperationClaim> operationClaims);
+
+    RefreshToken CreateRefreshToken(User user, string ipAddress);
+}
